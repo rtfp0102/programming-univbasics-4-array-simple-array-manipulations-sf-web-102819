c14e429446361ae07bd00eec9566205e63438711
rtfp0102 <rtfp0102@gmail.com>
2019-09-30 03:37:28 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -31,4 +31,5 @@ def using_insert(list_of_programming_languages, another_language )
   new_array = list_of_programming_languages.insert(4, another_language)
 end
 
-def using_unique()
+def using_uniq(haircuts)
+  new_array = haircuts.uniq
