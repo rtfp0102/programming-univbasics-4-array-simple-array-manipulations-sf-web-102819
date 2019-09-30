b71e38a9ece1aaec004a136a145609ef197d1fc8
rtfp0102 <rtfp0102@gmail.com>
2019-09-30 03:28:45 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -27,4 +27,5 @@ def using_concat(my_favorite_things, more_favs)
   all_my_favs = my_favorite_things.concat(more_favs)
 end
 
-def using_insert(list_of_programming_languages, )
+def using_insert(list_of_programming_languages, another_language )
+  
